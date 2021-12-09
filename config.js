383c2bab5253f239905933546f0afe7087ee41d0
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-12-09 19:42:29 +0100

    feat: Se añade opción para mostrar/ocultar la cabecera

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 // *** Valores de configuración por defecto
 
+// Mostrar la cabecera o no
+// default: var show_header = true;
+var show_header = true;
+
 // Enlace de la página por defecto
-// var home_url = "../piefirma/";
+// default: var home_url = "../piefirma/";
 var home_url = "https://hacolxclu.dphaco.ceh.junta-andalucia.es/piefirma/";
 
 
 // Aplicamos los valores de la configuración
+if ( ! show_header ) {
+    document.querySelector("#banner").style.display = 'none';
+}
 document.querySelector("#home_url").href = home_url;
